220df7da191e21fefe5f1e247a7e5df530535a15
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-04-06 03:23:18 +0600

    Products Page Done(3:02:40)

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -4,14 +4,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
     reducerPath: 'adminPath',
-    tagTypes: ['User'],
+    tagTypes: ['User', 'Products'],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
             providesTags: ["User"],
         }),
+        getProducts: build.query({
+            query: () => 'client/products',
+            providesTags: ['Products']
+        })
     })
 });
 
 
-export const { useGetUserQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
