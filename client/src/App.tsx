5e84038360f9300488971f5b3a8c9505417b1dd5
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-12-01 18:29:06 +0600

    [client] - redux store setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // mui
 import { CssBaseline } from '@mui/material';
 
 //project import
+import { store } from './store/store';
 import privateRoutes from './routes/privateRoutes';
 // import publicRoutes from './routes/publicRoutes';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <CssBaseline />
       <RouterProvider router={privateRoutes} />
-    </>
+    </Provider>
   );
 };
 
---
+++ client/src/store/store.ts
@@ -0,0 +1,20 @@
+import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
+import { taskApi } from './features/taskApi'
+import userReducer from './services/userSlice';
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    [taskApi.reducerPath]: taskApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([taskApi.middleware]),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
