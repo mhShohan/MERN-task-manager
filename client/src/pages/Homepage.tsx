93423bdc38f5a715bccad5cc526dee922ff53173
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-12-19 00:08:17 +0600

    [client] - format

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -8,6 +8,7 @@ const Homepage = () => {
       console.log(res);
     })();
   }, []);
+
   return <div>Homepage</div>;
 };
 
--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,13 +1,14 @@
-import axios from "axios";
-import config from './config'
-
+import axios from 'axios';
+import config from './config';
 
 const axiosSecure = axios.create({
   baseURL: config.baseUrl
-})
+});
 
 axiosSecure.interceptors.request.use((conf) => {
-  const token = localStorage.getItem('accessToken')
+  const token = localStorage.getItem('accessToken');
+
+
 
   if (token) {
     conf.headers.Authorization = `Bearer ${token}`;
@@ -15,17 +16,16 @@ axiosSecure.interceptors.request.use((conf) => {
 
 
 
-  return conf
-})
+
+  return conf;
+});
 
 export const getRequest = async (url: string) => {
   try {
-    const res = await axiosSecure.get(url)
+    const res = await axiosSecure.get(url);
 
-    return { data: res.data, error: null }
-
-
+    return { data: res.data, error: null };
   } catch (err: any) {
-    return { data: null, error: err.response.data }
+    return { data: null, error: err.response.data };
   }
-}
+};
